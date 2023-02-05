8408caa6fd5c6194f7860dbdeb4c23a8e2293ebb
dave <124534090+dave3x8@users.noreply.github.com>
2023-02-05 23:28:37 +0100

    Use local variables for API functions

--- Block server chat advertisements.lua
+++ Block server chat advertisements.lua
@@ -1,3 +1,6 @@
+-- local variables for API functions. any changes to the line below will be lost on re-generation
+local client_create_interface, client_set_event_callback, ui_get, ui_new_checkbox = client.create_interface, client.set_event_callback, ui.get, ui.new_checkbox
+
 local ffi = require 'ffi'
 ffi.cdef[[
   typedef bool(__thiscall *DispatchUserMessage_t)(void*, int messageType, int passthroughFlags, int size, const void* data);
@@ -5,14 +8,14 @@ ffi.cdef[[
 ]]
 
 -- Get a pointer to the interface table of the client module
-local interface = ffi.cast('uintptr_t**', client.create_interface('client.dll', 'VClient018'))
+local interface = ffi.cast('uintptr_t**', client_create_interface('client.dll', 'VClient018'))
 
 -- Get a pointer to the original function table
 local orig_table = ffi.cast('uintptr_t*', interface[0])
 
 -- Get the size of the original function table
 local table_size = 0
-while(orig_table[table_size] ~= 0x0) do table_size = table_size + 1 end
+while orig_table[table_size] ~= 0x0 do table_size = table_size + 1 end
 
 -- Create a new function table with the same size as the original one
 local hooked_table = ffi.new('uintptr_t[?]', table_size)
@@ -34,20 +37,20 @@ local blocked_messages = {
 -- Get the original dispatch user message function
 local oDispatch = ffi.cast('DispatchUserMessage_t', orig_table[38])
 
--- Create a menu checkbox to toggle
-local enabled = ui.new_checkbox('Misc', 'Miscellaneous', 'Block server chat advertisements')
+-- Create a menu checkbox to toggle whether it's enabled
+local enabled = ui_new_checkbox('Misc', 'Miscellaneous', 'Block server chat advertisements')
 
 -- Define the hook function for the dispatch user message function
 local function hkDispatch(this, type, flags, size, data)
   -- Return the result of the original dispatch user message function if the message type is not blocked, otherwise return true
-  return (blocked_messages[type] and ui.get(enabled)) and true or oDispatch(this, type, flags, size, data)
+  return (blocked_messages[type] and ui_get(enabled)) and true or oDispatch(this, type, flags, size, data)
 end
 
 -- Replace the original dispatch user message function with the hook function
 hooked_table[38] = ffi.cast('uintptr_t', ffi.cast('void*', ffi.cast('hkDispatchUserMessage_t', hkDispatch)))
 
 -- Set a shutdown event callback to restore the original function table
-client.set_event_callback('shutdown', function()
+client_set_event_callback('shutdown', function()
   hooked_table[38] = orig_table[38]
   interface[0] = orig_table
 end)
